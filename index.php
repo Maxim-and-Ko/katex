09f815ee8a449de1a4727f7e4404c8ead11905b7
Soul <45539750+soul-1023@users.noreply.github.com>
2020-07-12 21:37:01 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -13,39 +13,29 @@
 <? require_once("source/pages/main/footer.php"); ?>
 
 <script> 
-    const $ = (id) => document.getElementById(id) 
+    const $ = (id) => document.getElementById(id)
+    
+    function ViewMobileMenu () 
+    {
+        const menu = $('mm').classList;
+        const menuicon = $('menu-icon-folder').classList;
+
+        if (menu.contains("nonvisible")) 
+            menu.remove("nonvisible")
+        else 
+            menu.add("nonvisible") 
+
+        if (menuicon.contains("closed")) 
+            menuicon.remove("closed")
+        else 
+            menuicon.add("closed")
+
+    }
 </script>
 
 <script src="source/js/perehod.js"></script>
+<script src="source/js/anim_btn.js"></script>
+<script src="source/js/progress_bar.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="source/bootstrap/bootstrap.min.js"></script>
 <script src="https://kit.fontawesome.com/41ec05d543.js" crossorigin="anonymous"></script>
-<script src="source/js/anim_btn.js"></script>
-<script src="source/js/progress_bar.js"></script>
-<script>
-        function ViewMobileMenu () 
-        {
-            var menu = document.getElementById("mm").classList;
-
-            if (menu.contains("nonvisible")) 
-            {
-                menu.remove("nonvisible");
-            } 
-            else 
-            {
-                menu.add("nonvisible");
-            }
-
-            var menuicon = document.getElementById("menu-icon-folder").classList;
-
-            if (menuicon.contains("closed")) 
-            {
-                menuicon.remove("closed");
-            } 
-            else 
-            {
-                menuicon.add("closed");
-            }
-
-        }
-    </script>
